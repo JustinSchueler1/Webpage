7bf0c6006050ae4303ff4bbbcff30e66171b3235
JustinSchueler1 <111314262+JustinSchueler1@users.noreply.github.com>
2024-07-06 14:53:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const button = document.querySelector('.button');
+
+const button1 = document.getElementById('aboutMe');
+const button2 = document.getElementById('hobbies');
+const button3 = document.getElementById('goals');
+const button4 = document.getElementById('contact');
+const button5 = document.getElementById('resume');
+
+button.addEventListener('click', function() {
+    button.style.backgroundColor = '#ff5252';
+    button.style.boxShadow = '0 0 30px #ff5252';
+});
+
+button1.addEventListener('click', function() {
+    button1.style.backgroundColor = '#ff5252';
+    button1.style.boxShadow = '0 0 30px #ff5252';
+});
+
+button2.addEventListener('click', function() {
+    button2.style.backgroundColor = '#ff5252';
+    button2.style.boxShadow = '0 0 30px #ff5252';
+});
+
+button3.addEventListener('click', function() {
+    button3.style.backgroundColor = '#ff5252';
+    button3.style.boxShadow = '0 0 30px #ff5252';
+});
+
+button4.addEventListener('click', function() {
+    button4.style.backgroundColor = '#ff5252';
+    button4.style.boxShadow = '0 0 30px #ff5252';
+});
+
+button5.addEventListener('click', function() {
+    button5.style.backgroundColor = '#ff5252';
+    button5.style.boxShadow = '0 0 30px #ff5252';
+});
+
